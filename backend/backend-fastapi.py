85657029a8ff80229d1f15a98fa1d32a1a7b1a3d
Chinmay-mobio <chinmay.patel@mobiosolutions.com>
2025-06-13 13:08:22 +0530

    modified backend-python to solve str issue in expire token

--- backend/backend-fastapi.py
+++ backend/backend-fastapi.py
@@ -45,7 +45,7 @@ app.add_middleware(
 # Security configurations
 SECRET_KEY = os.getenv('SECRET_KEY')  # Change this in production
 ALGORITHM = os.getenv('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 # Initialize Groq Client
 client = Groq(api_key=os.getenv('GROQ_API_KEY'))
