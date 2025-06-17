9f5841a30e3524eec99ced8cd6cf6ca094159dd6
kishan-mobio <kishan.kalavadia@mobiosolutions.com>
2025-06-17 17:41:57 +0530

    fix: resolve token verify bug

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -26,7 +26,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
+        email: str = payload.get("email")
         if email is None:
             raise credentials_exception
         token_data = TokenData(username=email)
